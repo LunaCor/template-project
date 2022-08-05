5cd12d5ec7b41eda4fc3766099c624e15896411f
Emanuel Vallejo <emanuelvallejo@Emanuels-MacBook-Pro.local>
2022-08-05 05:17:35 +0200

    Base any project

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import 'module-alias/register';
+import fn from '@services/service';
+console.log(fn());
+console.log('Hello world from base configuration!');
---
+++ src/services/service.ts
@@ -0,0 +1,9 @@
+
+
+const fn = () => {
+    return {
+        saludo : "I'm a service"
+    }
+}
+
+export default fn;
